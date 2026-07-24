1c2ead86e1475085ddf8b31fea961afe90c7a87d
yzndev <mohammadygit@gmail.com>
2026-07-25 00:59:33 +0330

    Feature: Add api routes in api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\AppointmentController;
+use App\Http\Controllers\Api\DoctorController;
+use App\Http\Controllers\Api\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/services', [ServiceController::class, 'index']);
+Route::get('/services/{slug}', [ServiceController::class, 'show']);
+
+Route::get('/doctors', [DoctorController::class, 'index']);
+
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{slug}', [ArticleController::class, 'show']);
+#----------------------------------------------------------------------------------------
+Route::post('/appointments', [AppointmentController::class, 'store']);
