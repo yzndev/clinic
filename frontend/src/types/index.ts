9bdf00a834484e6aa52cb19aff54d690119ff972
yzndev <mohammadygit@gmail.com>
2026-07-25 16:40:29 +0330

    Feature: install tailwindcss - add directories for structure - add types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,34 @@
+// src/types/index.ts
+
+export interface Service {
+    id: number;
+    title: string;
+    slug: string;
+    description: string;
+    image: string | null;
+    is_active: boolean;
+}
+
+export interface Doctor {
+    id: number;
+    name: string;
+    specialty: string;
+    bio: string;
+    image: string | null;
+}
+
+export interface Article {
+    id: number;
+    title: string;
+    slug: string;
+    body: string;
+    image: string | null;
+    is_published: boolean;
+}
+
+export interface Appointment {
+    full_name: string;
+    phone_number: string;
+    service_id?: number | null;
+    message?: string;
+}
